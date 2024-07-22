bf74d9742bde4406494e43b1e6426f68144eef8d
yousefsaad0 <ysaad2721@gmail.com>
2024-07-22 15:40:51 +0300

    utilized FindByIdDto for request parameters, and updated imports, added exception layer to ProductsService

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Patch, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { RolesGuard } from 'src/users/guards/roles.guard';
@@ -9,6 +9,8 @@ import { UpdateProductDto } from './dtos/update-product.dto';
 import { ApiBadRequestResponse, ApiBearerAuth, ApiBody, ApiCreatedResponse, ApiForbiddenResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { GetQueryParamsDto } from 'src/common/dtos/get-page-query-params.dto';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { FindByIdDto } from 'src/common/dtos/find-by-id.dto';
 
 @ApiTags("Products")
 @ApiBearerAuth("JWT-auth")
@@ -35,8 +37,8 @@ export class ProductsController {
     @ApiParam({ name: 'productId', description: 'Product ID', type: 'string' })
     @ApiUnauthorizedResponse({ description: 'You need to be logged-in to access this resource.' })
     @Get('/:productId')
-    async getProductById(productId:string): Promise<Product>{
-        return await this.productsService.findProductById(productId)
+    async getProductById(@Param('productId') findProductByIdDto:FindByIdDto): Promise<Product>{
+        return await this.productsService.findProductById(findProductByIdDto)
     }
 
     @ApiOperation({ summary: 'Create a new product' })
@@ -47,8 +49,8 @@ export class ProductsController {
     @ApiBody({ description: 'Product data to create', type: CreateProductDto })
     @Roles(Role.Admin)
     @Post('/')
-    async createProduct(@Body() createProductDto:CreateProductDto){
-        return await this.productsService.createProduct(createProductDto)
+    async createProduct(@CurrentUser() creatorId:string, @Body() createProductDto:CreateProductDto):Promise<Product>{
+        return await this.productsService.createProduct(creatorId, createProductDto)
     }
 
     @ApiOperation({ summary: 'Update a product by ID' })
@@ -61,8 +63,8 @@ export class ProductsController {
     @ApiBody({ description: 'Product data to update', type: UpdateProductDto })
     @Roles(Role.Admin)
     @Patch('/:productId')
-    async updateProductById(productId:string, @Body() updateProductDto:UpdateProductDto):Promise<Product>{
-        return this.productsService.updateProduct(productId, updateProductDto)
+    async updateProductById(@CurrentUser() currentUserId:string, @Param('productId') findProductByIdDto:FindByIdDto, @Body() updateProductDto:UpdateProductDto):Promise<Product>{
+        return await this.productsService.updateProduct(currentUserId, findProductByIdDto, updateProductDto)
     }
 
     @ApiOperation({ summary: 'Delete a product by ID' })
@@ -74,7 +76,7 @@ export class ProductsController {
     @Roles(Role.Admin)
     @UseGuards(RolesGuard)
     @Delete('/:productId')
-    async deleteProductById(productId:string):Promise<Product>{
-        return this.productsService.deleteProduct(productId)
+    async deleteProductById(@Param('productId') findProductByIdDto:FindByIdDto):Promise<Product>{
+        return this.productsService.deleteProduct(findProductByIdDto)
     }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dtos/create-product.dto';
-import { Product, ProductSchema } from './schemas/products.schema';
+import { Product } from './schemas/products.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UpdateProductDto } from './dtos/update-product.dto';
 import { GetQueryParamsDto } from 'src/common/dtos/get-page-query-params.dto';
+import { UserInterface } from 'src/users/interfaces/user.interface';
+import { FindByIdDto } from 'src/common/dtos/find-by-id.dto';
 
 @Injectable()
 export class ProductsService {
@@ -17,21 +19,31 @@ export class ProductsService {
         return await this.productModel.find().skip(offset).limit(skip).exec()
     }
     
-    async createProduct(createProductDto: CreateProductDto): Promise<Product> {
+    async createProduct(creatorId:string, createProductDto: CreateProductDto): Promise<Product> {
+        createProductDto.createdBy = creatorId
         const createdProduct = new this.productModel({ ...createProductDto });
         return await createdProduct.save();
     }
 
-    async findProductById(productId:string): Promise<Product>{
-        return await this.productModel.findById(productId)
+    async findProductById(productId:FindByIdDto): Promise<Product>{
+        const product = await this.productModel.findById(productId.id)
+        if(!product){throw new NotFoundException('Product not found')}
+        return product
     }
 
-    async updateProduct(productId:string, updateProductDto:UpdateProductDto): Promise<Product>{
-        return await this.productModel.findByIdAndUpdate(productId, updateProductDto)
+    async updateProduct(currentUserId:string, productId:FindByIdDto, updateProductDto:UpdateProductDto): Promise<Product>{
+        const product = await this.productModel.findById(productId.id)
+        if(!product){throw new NotFoundException('Product not found')}
+        const creatorId = product.createdBy as UserInterface
+        if(currentUserId!==(creatorId._id).toString()){throw new ForbiddenException("Cannot update other Users' products")}
+        const updatedProduct = await this.productModel.findByIdAndUpdate(productId.id,updateProductDto)
+        return updatedProduct
     }
 
-    async deleteProduct(productId:string):Promise<Product>{
-        return await this.productModel.findByIdAndDelete(productId)
+    async deleteProduct(productId:FindByIdDto):Promise<Product>{
+        const product = await this.productModel.findByIdAndDelete(productId.id)
+        if(!product){throw new NotFoundException('Product not found')}
+        return product
     }
 
     // Add any other relevant methods...
