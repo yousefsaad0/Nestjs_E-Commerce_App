d70141bd12dad986f4b348d5bd81d9ff8922c3c6
yousefsaad0 <ysaad2721@gmail.com>
2024-07-22 15:38:32 +0300

    modified this dto to be more general in its use

--- src/common/dtos/find-by-id.dto.ts
+++ src/common/dtos/find-by-id.dto.ts
@@ -1,8 +1,8 @@
 import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class FindUserByIdDto {
+export class FindByIdDto {
   @ApiProperty({ type:'string',example: '60d21b4667d0d8992e610c85' })
   @IsString()
-  userId: string;
+  id: string;
 }
