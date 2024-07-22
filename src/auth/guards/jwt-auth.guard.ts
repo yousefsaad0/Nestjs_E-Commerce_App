99aa324e4db70654a5ed2c84626c2cb4506ec3f7
yousefsaad0 <ysaad2721@gmail.com>
2024-07-22 12:18:22 +0300

    modified guard's handleRequest implementation to return clearer error message

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+    handleRequest(err, user, info) {
+        if (err || !user) {
+          throw err || new UnauthorizedException({ "statusCode": 401,
+            "message": 'You need to be logged-in to access this resource.',
+            "error": "Unauthorized"});
+        }
+        return user;
+      }
+}
